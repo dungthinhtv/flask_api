b4954930b94dd9ef81e905b7f1cac21fc462b436
dungthinhtv <dungthinhtv@gmail.com>
2023-04-04 12:53:50 +0700

    asdasd

--- main.py
+++ main.py
@@ -51,14 +51,14 @@ def query_records():
 @app.route('/mv/', methods=['PUT'])
 def create_record():
     record = json.loads(request.data)
-    with open('/tmp/movie_full_src_999.json', 'r') as f:
+    with open('/tmp/movie_title_100_detail.json', 'r') as f:
         data = f.read()
     if not data:
         records = [record]
     else:
         records = json.loads(data)
         records.append(record)
-    with open('/tmp/movie_full_src_999.json', 'w') as f:
+    with open('/tmp/movie_title_100_detail.json', 'w') as f:
         f.write(json.dumps(records, indent=2))
     return jsonify(record)
 
@@ -66,14 +66,14 @@ def create_record():
 def update_record():
     record = json.loads(request.data)
     new_records = []
-    with open('/tmp/movie_full_src_999.json', 'r') as f:
+    with open('/tmp/movie_title_100_detail.json', 'r') as f:
         data = f.read()
         records = json.loads(data)
     for r in records:
         if r['name'] == record['name']:
             r['email'] = record['email']
         new_records.append(r)
-    with open('/tmp/movie_full_src_999.json', 'w') as f:
+    with open('/tmp/movie_title_100_detail.json', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
     return jsonify(record)
     
@@ -81,14 +81,14 @@ def update_record():
 def delte_record():
     record = json.loads(request.data)
     new_records = []
-    with open('/tmp/movie_full_src_999.json', 'r') as f:
+    with open('/tmp/movie_title_100_detail.json', 'r') as f:
         data = f.read()
         records = json.loads(data)
         for r in records:
             if r['name'] == record['name']:
                 continue
             new_records.append(r)
-    with open('/tmp/movie_full_src_999.json', 'w') as f:
+    with open('/tmp/movie_title_100_detail.json', 'w') as f:
         f.write(json.dumps(new_records, indent=2))
     return jsonify(record)
 
